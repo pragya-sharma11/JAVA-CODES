d6e4613a77958d6cfcdf9bd3dd295b6f76d81257
pragya-sharma11 <67971009+pragya-sharma11@users.noreply.github.com>
2020-08-06 00:31:10 +0530

    Create OnlineTicketBooking.java

---
+++ OnlineTicketBooking.java
@@ -0,0 +1,36 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Pragya Sharma
+ *   Date: 01/08/20
+ *   Time: 4:30 AM
+ *   File: Lab4Task.java
+ */
+import java.util.Scanner;
+
+// Software to book tickets for online conference.
+public class OnlineTicketBooking {
+    private static final byte TOTAL_PARTICIPANTS = 100;
+
+    private static void sellTicket(String participantName) {
+        String message = "Congratulations, " + participantName + ". " +
+                "See you at the conference!";
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        byte numberOfTickectsSold = 0;
+        while (numberOfTickectsSold <= TOTAL_PARTICIPANTS) {
+            byte ticketsLeft = (byte) (TOTAL_PARTICIPANTS - numberOfTickectsSold);
+            System.out.println("Total Tickets Left: " + ticketsLeft);
+            System.out.println("Enter your name for ticket in Online Conference");
+            Scanner scanner = new Scanner(System.in);
+            String nameOfParticipant = scanner.nextLine();
+            sellTicket(nameOfParticipant);
+            numberOfTickectsSold++;
+            if (numberOfTickectsSold == TOTAL_PARTICIPANTS) {
+                scanner.close();
+                System.out.println("Sorry, The tickets are sold-out!");
+                break;
+            }
+        }
+    }
+}
