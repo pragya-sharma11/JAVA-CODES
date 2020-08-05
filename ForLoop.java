bfa028e9102b87088ab779bbf7fa7deac57a52c5
pragya-sharma11 <67971009+pragya-sharma11@users.noreply.github.com>
2020-08-06 00:46:43 +0530

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+// 3 types of for loop initialisation!!!!
+public class ForLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[10];
+        //type 1 :- simple for loop
+        System.out.println("Enter the elements for an integer type array!!!");
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = scanner.nextInt();
+        }
+        scanner.close();
+        //type 2:- foreach loop with IntStream!!!!
+        IntStream.range(0, numbers.length).forEach(index -> numbers[index] = numbers[index] * 2);
+        //type 2:- forEach loop in simple way!!!!
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
