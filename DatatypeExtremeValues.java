b4cf03f801068d1f8fdc4e5d2c221d64944fa3db
pragya-sharma11 <67971009+pragya-sharma11@users.noreply.github.com>
2020-08-06 01:16:46 +0530

    Create DatatypeExtremeValues.java

---
+++ DatatypeExtremeValues.java
@@ -0,0 +1,19 @@
+/*
+ * Created by intellij IDEA
+ *Author :Pragya Sharma
+ * Date :20-July-2020
+ * Time:10:41 pm
+ * File Name: DatatypeExtremeValues */
+package labWork;
+
+public class DatatypeExtremeValues {
+    public static void main(String[] args) {
+        System.out.println("Minimum Value of Byte: "+Byte.MIN_VALUE+" and Maximum Value of Byte: "+Byte.MAX_VALUE);
+        System.out.println("Minimum Value of Short: "+Short.MIN_VALUE+" and Maximum Value of Short: "+Short.MAX_VALUE);
+        System.out.println("Minimum Value of Integer: "+Integer.MIN_VALUE+" and Maximum Value of Integer: "+Integer.MAX_VALUE);
+        System.out.println("Minimum Value of Long: "+Long.MIN_VALUE+" and Maximum Value of Long: "+Long.MAX_VALUE);
+        System.out.println("Minimum Value of Float: "+Float.MIN_VALUE+" and Maximum Value of Float: "+Float.MAX_VALUE);
+        System.out.println("Minimum Value of Double "+Double.MIN_VALUE+" and Maximum Value of Double: "+Double.MAX_VALUE);
+        System.out.println("Minimum Value of Char: "+(int)Character.MIN_VALUE+" and Maximum Value of Char: "+(int)Character.MAX_VALUE);
+    }
+}
