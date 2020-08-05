ba1b8d9dbfddafb18cce520bd395c892356eb2c7
pragya-sharma11 <67971009+pragya-sharma11@users.noreply.github.com>
2020-08-06 01:14:05 +0530

    Update CollatzConjecture.java

--- CollatzConjecture.java
+++ CollatzConjecture.java
@@ -2,7 +2,7 @@
  *   Author: Pragya Sharma
  *   Date: 27/07/20
  *   Time: 2:38 PM
- *   File: Lab3Task.java
+ *   File: CollatzConjecture.java
  */
 import java.util.Scanner;
 class CollatzConjecture {
