df0e126230ce887064a96abec87368e31023078b
pragya-sharma11 <67971009+pragya-sharma11@users.noreply.github.com>
2020-08-06 17:12:45 +0530

    Create LocalTypeInferencing.java

---
+++ LocalTypeInferencing.java
@@ -0,0 +1,27 @@
+/*
+ * Created by intellij IDEA
+ *Author :Pragya Sharma
+ * Date :25-July-2020
+ * File Name: LocalTypeInferencing */
+
+public class LocalTypeInferencing {
+    public static void main(String[] args) {
+        //making program  for purchasing laptop i.e object is laptop.
+        var ramInGb = (byte) 4;
+        var processor = (short) 64;
+        var numberOfsoftwaresInstalled = 4;
+        var priceOfLaptop = 40000L;
+        var discount = 4.998;
+        var priceAfterDiscout = 38000.5f;
+        char opertingSystemWindowsOrLinux = 'W';
+        boolean wannaBuy = true;
+        System.out.println("Ram is " + ramInGb + " GB");
+        System.out.println("processor is " + processor + " bit");
+        System.out.println("price of laptop is Rs." + priceOfLaptop);
+        System.out.println("discount is " + discount + "%");
+        System.out.println("price after discount is " + priceAfterDiscout);
+        System.out.println("operating system W for windows nd L for Linux" + opertingSystemWindowsOrLinux);
+        System.out.println("Do you want to buy??-->" + wannaBuy);̥
+*/
+    }
+}
