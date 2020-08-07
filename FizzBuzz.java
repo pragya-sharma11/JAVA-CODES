430780fb676d3601e3de7b6b69ad6dcfb7b4bd9a
pragya-sharma11 <67971009+pragya-sharma11@users.noreply.github.com>
2020-08-07 19:29:00 +0530

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,8 +3,8 @@
  *Author :Pragya Sharma
  * Date :25-July-2020
  * File Name: FizzBuzz */
-package labWork;
 import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
