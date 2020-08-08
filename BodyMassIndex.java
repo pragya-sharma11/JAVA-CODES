7f34ac9f1a436d5eb76d9324e323788921779c87
pragya-sharma11 <67971009+pragya-sharma11@users.noreply.github.com>
2020-08-09 01:28:38 +0530

    Update BodyMassIndex.java

--- BodyMassIndex.java
+++ BodyMassIndex.java
@@ -19,6 +19,23 @@ public class BodyMassIndex {
         float weight = scanner.nextFloat();
         float heightInMetres = (feetHeight * 0.3048f) + (inchesHeight * 0.0254f);
         float bodyMassIndex = weight / (heightInMetres * heightInMetres);
-        System.out.print("Body Mass Index of Your Body is : " + bodyMassIndex);
+        System.out.println("Body Mass Index of Your Body is : " + bodyMassIndex);
+        if (bodyMassIndex < 15.0f) {
+            System.out.println("You are very severely underweight!!!");
+        } else if (bodyMassIndex >= 15.0f && bodyMassIndex <= 16.0f) {
+            System.out.println("You are severely underweight!!!");
+        } else if (bodyMassIndex > 16.0f && bodyMassIndex <= 18.4f) {
+            System.out.println("You are Underweight!!!");
+        } else if (bodyMassIndex > 18.4f && bodyMassIndex <= 24.9) {
+            System.out.println("You are Normal Weight!!!");
+        } else if (bodyMassIndex >= 25.0f && bodyMassIndex <= 29.0f) {
+            System.out.println("You are OverWeight!!!");
+        } else if (bodyMassIndex >= 30.0f && bodyMassIndex <= 34.9) {
+            System.out.println("You are moderately obese!!!");
+        } else if (bodyMassIndex >= 35.0f && bodyMassIndex <= 39.9f) {
+            System.out.println("You are Severely Obese!!!");
+        } else {
+            System.out.println("You are Morbidly obese!!!");
+        }
     }
 }
